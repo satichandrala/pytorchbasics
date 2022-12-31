bb0c9401eb31b47856a3d62dfb889e368c1fd732
Purna <satichandrala@gmail.com>
2022-12-31 12:59:19 +0100

    transferlearning-trainingloop

--- transferlearning/transferlearningusingpytorch.py
+++ transferlearning/transferlearningusingpytorch.py
@@ -41,4 +41,43 @@ image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
 dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
                                             shuffle=true, num_workers=4)
                     for x in ['train', 'val']}
-                    
+dataset_sizes = {x: len(image_datasets[x] for x in ['train', 'val'])}
+class_names = image_datasets['train'].classes
+print(class_names)
+# training of the model and evaluation
+def train_model(model, criterion, optimizer, scheduler, num_epochs = 25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs-1}')
+        print('-'*10)
+# here we are doing training and validation in each epoch
+
+    for phase in ['train', 'val']:
+        if phase == 'train':
+            model.train()
+        else:
+            model.eval()
+
+        running_loss = 0.0
+        running_corrects = 0
+        # iterate over the data via loop, for the inputs and labels
+        for inputs, labels in dataloaders[phase]:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # forward loop , track history only if in training phase
+            with torch.set_grad_enabled(phase == 'train'):
+                outputs = model(inputs)
+                _, preds = torch.max(outputs, 1)
+                loss = criterion(outputs, labels)
+
+                # backward and optimize only if in training phase
+                if phase == 'train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+            # 
