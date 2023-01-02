7b54420d6c40abd20cc9625c707888844544e898
Purna <satichandrala@gmail.com>
2023-01-02 15:41:54 +0100

    creating a checkpoint and loading it

--- saveandload-Models/saveload.py
+++ saveandload-Models/saveload.py
@@ -12,20 +12,43 @@ class Model(nn.Module):
         return y_pred
 
 model = Model(n_input_features=6)
+print(model.state_dict())# check how state_dict looks like:
+# train the model
+learning_rate = 0.001
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+print(optimizer.state_dict())
+# saving and loading a checkpoint
+checkpoint = {
+    "epoch": 90,
+    "model_state": model.state_dict(),
+    "optim_state": optimizer.state_dict()
+}
 
-for param in model.parameters():
-    print(param)
-# train your model... 
+#torch.save(checkpoint, ".\pytorchbasics\saveandload-Models\checkpoint.pth")
+loaded_checkpoint = torch.load(".\pytorchbasics\saveandload-Models\checkpoint.pth")
+epoch = loaded_checkpoint["epoch"]
 
-FILE = ".\pytorchbasics\saveandload-Models\model.pth" # .pth is the common format to use meaning pytorch
-torch.save(model.state_dict(), FILE)
+model = Model(n_input_features=6)
+optimizer = torch.optim.SGD(model.parameters(), lr=0)
 
-# now we load our model from FILE
-# model = torch.load(FILE)
-# model.eval() # evaluation of model
-loaded_model = Model(n_input_features=6)
-loaded_model.load_state_dict(torch.load(FILE))
-loaded_model.eval()
-# using model to inspect a parameter
-for param in loaded_model.parameters():
-    print(param)
+model.load_state_dict(checkpoint["model_state"])
+optimizer.load_state_dict(checkpoint["optim_state"])
+
+print(optimizer.state_dict())
+
+# for param in model.parameters():
+#     print(param)
+# # train your model... 
+
+# FILE = ".\pytorchbasics\saveandload-Models\model.pth" # .pth is the common format to use meaning pytorch
+# torch.save(model.state_dict(), FILE)
+
+# # now we load our model from FILE
+# # model = torch.load(FILE)
+# # model.eval() # evaluation of model
+# loaded_model = Model(n_input_features=6)
+# loaded_model.load_state_dict(torch.load(FILE))
+# loaded_model.eval()
+# # using model to inspect a parameter
+# for param in loaded_model.parameters():
+#     print(param)
