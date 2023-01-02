6868a6298d396749f8c9c03c324b12da7da0a43a
Purna <satichandrala@gmail.com>
2023-01-02 16:54:22 +0100

    saving and loading models on a CPU and GPU viz

---
+++ saveandload-Models/loadingmodelsonCPUAndGPU.py
@@ -0,0 +1,9 @@
+# save model on GPU and load on GPU
+
+device = torch.device("cuda")
+model.to(device)
+torch.save(model.state_dict(), PATH)
+
+model = Model(*args, **kwargs)
+model.load_state_dict(torch.load(PATH))
+model.to(device)
